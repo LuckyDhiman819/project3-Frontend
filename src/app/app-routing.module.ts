a2d68afb427f4936797660835fafa3841a2f603e
ketan shinde <ssanket2001@gmail.com>
2021-09-19 16:26:33 +0530

    added customer dashboard byt ketan

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BillingComponent } from './components/billing/billing.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"",component: HomeComponent },
+  { path:'billing',component: BillingComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
